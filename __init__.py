20e9f3f4256f55079c3a290e47b3505d1c511632
glushkov <glushkov@skbkontur.ru>
2017-04-16 21:21:25 +0500

    Fixes and additions to FeaturePreparer

--- __init__.py
+++ __init__.py
@@ -15,30 +15,47 @@ class FeaturePreparer:
         self.powers = powers or [1]
         self.scaler = preprocessing.StandardScaler()
         self.__orig_std = None # needed for scaling features before taking exp
+        self._non_boolean_features_mask = None # 1d mask array that needed for skipping redundant log and exp
 
     def apply_powers(self, x):
-        f_list = [x ** p for p in self.powers if type(p) is int]
+        f_list = []
+        if 1 in self.powers:
+            f_list += [x]
+        non_boolean_features = x[:,self._non_boolean_features_mask]
+        f_list += [non_boolean_features ** p for p in self.powers if type(p) is int and p != 1]
         if "perm" in self.powers:
             f_list += [x[:, i:i+1] * x[:, j:j+1] for i in range(x.shape[1]) for j in range(i + 1)]
         if "log" in self.powers:
-            f_list += [np.log(x + 1)]
+            f_list += [np.log(non_boolean_features + 1)]
         if "exp" in self.powers:
-            f_list += [np.exp(x / self.__orig_std)]
+            f_list += [np.exp(non_boolean_features / self.__orig_std)]
         return np.concatenate(f_list, axis=1) if len(f_list) > 1 else f_list[0]
 
-    def fit(self, x):
-        self.__orig_std = x.std(axis=0)
+    def __fit(self, x):
+        self._non_boolean_features_mask = np.array([len(np.unique(col)) > 2 for col in x.T])
+        non_boolean_features = x[:,self._non_boolean_features_mask]
+        self.__orig_std = non_boolean_features.std(axis=0)
         x_new = self.apply_powers(x)
-        return self.scaler.fit(x_new)
+        self.scaler.fit(x_new)
+        return x_new
+
+    def fit(self, x):
+        self.__fit(x)
+        return self
 
     def transform(self, x):
         x_new = self.apply_powers(x)
         return self.scaler.transform(x_new)
 
     def fit_transform(self, x):
-        self.__orig_std = x.std(axis=0)
-        x_new = self.apply_powers(x)
-        return self.scaler.fit_transform(x_new)
+        x_new = self.__fit(x)
+        return self.scaler.transform(x_new)
+
+    def expand_columns_names_list(self, columns):
+        non_boolean_columns = columns[self._non_boolean_features_mask]
+        prefixes = [str(p) for p in self.powers if p != 1]
+        additional_columns = np.array([pref + "_"+ col for pref in prefixes for col in non_boolean_columns])
+        return np.concatenate([columns, additional_columns])
 
 
 def plot_learning_curves(est, X, y, title, ylim=(.6, 1), cv=3, train_sizes=np.linspace(.05, 1, 10)):
